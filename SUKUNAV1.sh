a169b7c88a64d646c98400b9e90f26fc1e44949a
katagirikun <swapnilkolate044@gmail.com>
2025-03-20 15:37:16 +0530

    Create SUKUNAV1.sh

---
+++ SUKUNAV1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "✅ SUKUNAV1 script is running!"
+curl -o "/storage/emulated/0/Download/CROZNXSKINS/game_patch_3.7.0.19735.pak" -L "https://download1530.mediafire.com/f3zmsm4na8og7wXK6jASCsN-bRjN_myZK8J4mm4rW0QM9gpOvkJHh2NE28BckfYl9xiHDkFaCQq2GOmj3m9O60_B5qiihwL8mt42tKyll3te9KUAfnza9ta2Ra0sSVxVmG5cAWr7X433Za1d1X25Zc82gv_-AIn6Y81Pm8fHgGs/7dkwm3yjqx0q1q9/game_patch_3.7.0.19735.pak"
+echo "✅ Download Complete!"
